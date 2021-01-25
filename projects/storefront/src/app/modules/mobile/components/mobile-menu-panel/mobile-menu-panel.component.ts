481c8f1a52a7d12a7ef47d5fa237df5781e3b854
ammych <77796939+ammych@users.noreply.github.com>
2021-01-25 19:49:58 +0530

    Add files via upload

---
+++ projects/storefront/src/app/modules/mobile/components/mobile-menu-panel/mobile-menu-panel.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostBinding, Input, TemplateRef } from '@angular/core';
+import { MobileMenuService } from '../../../../services/mobile-menu.service';
+
+@Component({
+    selector: 'app-mobile-menu-panel',
+    templateUrl: './mobile-menu-panel.component.html',
+})
+export class MobileMenuPanelComponent {
+    @Input() level = 0;
+
+    @Input() label = '';
+
+    @Input() content: TemplateRef<any>;
+
+    @HostBinding('class.mobile-menu__panel') classMobileMenuPanel = true;
+
+    @HostBinding('style.transform') get styleTransform(): string {
+        return `translateX(${this.level * 100}%)`;
+    }
+
+    constructor(
+        public menu: MobileMenuService,
+    ) { }
+}
