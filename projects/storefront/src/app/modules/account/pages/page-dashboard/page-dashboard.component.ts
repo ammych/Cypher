1781a33030c9343f487bd0aa2365d25be0a0c668
ammych <77796939+ammych@users.noreply.github.com>
2021-01-25 19:41:34 +0530

    Add files via upload

---
+++ projects/storefront/src/app/modules/account/pages/page-dashboard/page-dashboard.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AccountApi } from '../../../../api/base';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Address } from '../../../../interfaces/address';
+import { UrlService } from '../../../../services/url.service';
+import { Order } from '../../../../interfaces/order';
+
+@Component({
+    selector: 'app-page-dashboard',
+    templateUrl: './page-dashboard.component.html',
+})
+export class PageDashboardComponent implements OnInit, OnDestroy {
+    private destroy$: Subject<void> = new Subject<void>();
+
+    address: Address;
+
+    orders: Order[] = [];
+
+    constructor(
+        public account: AccountApi,
+        public url: UrlService,
+    ) { }
+
+    ngOnInit(): void {
+        this.account.getDefaultAddress().pipe(takeUntil(this.destroy$)).subscribe(x => this.address = x);
+        this.account.getOrdersList({limit: 3}).pipe(takeUntil(this.destroy$)).subscribe(x => this.orders = x.items);
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+}
