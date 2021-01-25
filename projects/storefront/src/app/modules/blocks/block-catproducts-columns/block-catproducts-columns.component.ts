80cb34476c7ad4f2f750c5dd8c4b416fa9c6145c
ammych <77796939+ammych@users.noreply.github.com>
2021-01-25 19:43:54 +0530

    Add files via upload

---
+++ projects/storefront/src/app/modules/blocks/block-catproducts-columns/block-catproducts-columns.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { Product } from '../../../interfaces/product';
+
+export interface BlockCatproductsColumnsItem {
+    title: string;
+    products: Product[];
+	bannerimg: string;
+}
+
+@Component({
+    selector: 'app-block-catproducts-columns',
+    templateUrl: './block-catproducts-columns.component.html',
+})
+export class BlockCatproductsColumnsComponent {
+    @Input() columns: BlockCatproductsColumnsItem[] = [];
+
+    @HostBinding('class.block') classBlock = true;
+
+    @HostBinding('class.block-products-columns') classBlockProductsColumns = true;
+
+    constructor() { }
+}
