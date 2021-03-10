52dc42bab927a00ac517a559c4a198107271d6d9
ammych <77796939+ammych@users.noreply.github.com>
2021-03-10 15:57:26 +0530

    Add files via upload

--- projects/storefront/src/app/modules/sidenav/components/sidenav/sidenav.component.ts
+++ projects/storefront/src/app/modules/sidenav/components/sidenav/sidenav.component.ts
@@ -1,4 +1,8 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, OnDestroy, HostBinding } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { AccountApi } from '../../../../api/base';
+import { Router } from '@angular/router';
 import { sideMenu } from '../../../../../data/header-side-menu';
 import { SideMenuLink } from '../../../../interfaces/side-menu-link';
 import { HeaderService } from '../../../../services/header.service';
@@ -9,18 +13,26 @@ import { HeaderService } from '../../../../services/header.service';
 })
 
 export class SidenavComponent {
+	private destroy$: Subject<void> = new Subject<void>();
     items: SideMenuLink[] = sideMenu;
-
+	
     hoveredItem;
 
     @HostBinding('class.side-menu') classSideMenu = true;
 
     constructor(
+		public account: AccountApi,
         public header: HeaderService,
+		private router: Router,
     ) {}
 
     onItemEnter(item: any): void {
         this.hoveredItem = item;
+    }
+	logout(): void {
+        this.account.signOut().pipe(takeUntil(this.destroy$)).subscribe(() => {
+            this.router.navigateByUrl('/account/login').then();
+        });
     }
 
     onItemLeave(item: any): void {
--- projects/storefront/src/app/modules/sidenav/sidenav.module.ts
+++ projects/storefront/src/app/modules/sidenav/sidenav.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 // modules (angular)
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CollapseModule } from '../collapse';
 // components
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
@@ -21,6 +22,7 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
         // modules (angular)
         CommonModule,
 		RouterModule,
+		CollapseModule,
     ],
 	
 })
